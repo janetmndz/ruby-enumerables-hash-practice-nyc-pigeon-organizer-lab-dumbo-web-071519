84455f8c9f2671143436f94458f180c97e806817
Janet Mendez <janetmendez.03@gmail.com>
2019-07-03 05:41:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,15 @@
 def nyc_pigeon_organizer(data)
   data.keys.reduce({}) {|memo, category|
+  
     data[category].each{|(key, val)|
+    
       val.each{|pigeon_name|
+      
         ### if the pigeon doesn't have an existing entry, add it
         if !memo.include?(pigeon_name)
           memo[pigeon_name] = { category => ["#{key.to_s}"] }
         else
+          
           ### push new value in existing category entry
           if memo[pigeon_name][category]
             memo[pigeon_name][category].push("#{key.to_s}")
@@ -13,6 +17,7 @@ def nyc_pigeon_organizer(data)
           else 
              memo[pigeon_name][category] = ["#{key.to_s}"]
           end
+          
         end
       }
     }
