d3faf8db49dd3db34ade98c4e5f79505c9bccac7
Janet Mendez <janetmendez.03@gmail.com>
2019-07-03 04:36:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.keys.each {|category|
-    data[category].each {|(key,val))|
+    data[category].each {|(key, val)|
       p key
       p val
     }
