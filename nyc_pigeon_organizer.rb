dce5289913930b459f82e7e04cf8c17f4a011114
Janet Mendez <janetmendez.03@gmail.com>
2019-07-03 04:56:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
       while count < val.length do
         if !pigeon_list.include?(val[count])
           pigeon_list[val[count]] = {
-            category => key.to_s
+            category => [ key.to_s ]
           }
         else
           pigeon_list[val[count]][category] = pigeon_list[val[count]][category] ? pigeon_list[val[count]][category] += key.to_s : key.to_s
