212d1df1b7df7721ca776fa468393cc80ea2ae6e
Janet Mendez <janetmendez.03@gmail.com>
2019-07-03 05:21:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,24 +1,6 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  data.keys.each {|category|
-    data[category].each {|(key, val)|
-      count = 0
-      while count < val.length do
-        pigeon_name = val[count]
-        if !pigeon_list.include?(pigeon_name)
-          pigeon_list[pigeon_name] = {
-            category => ["#{key.to_s}"]
-          }
-        else
-          if pigeon_list[pigeon_name][category]
-            pigeon_list[pigeon_name][category].push("#{key.to_s}")
-          else 
-             pigeon_list[pigeon_name][category] = ["#{key.to_s}"]
-          end
-        end
-        count += 1
-      end
-    }
+  data.keys.reduce({}) {|memo, (key, value)|
+    p key
+    p value
   }
-  p pigeon_list
 end
