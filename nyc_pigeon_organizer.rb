ff502c9afa89f2d88f37f3894acb8313e2bb969e
Janet Mendez <janetmendez.03@gmail.com>
2019-07-03 05:10:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,7 +15,7 @@ def nyc_pigeon_organizer(data)
           }
         else
           p "inside else"
-          p pigeon_list[pigeon_name[category]]
+          p pigeon_list[pigeon_name][category]
         end
         count += 1
       end
