046df357e7c5303d2e4b12550c9dfc6016fa3ddd
Janet Mendez <janetmendez.03@gmail.com>
2019-07-03 05:20:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,7 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.keys.each {|category|
-    #p category ### returns color/gender/lives
     data[category].each {|(key, val)|
-      #p key ## returns sub categories
-      #p val ## returns array
-      
       count = 0
       while count < val.length do
         pigeon_name = val[count]
